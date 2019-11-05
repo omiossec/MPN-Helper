8b031b20fcb21a2848c2a3609b5c7b7e333d8d6a
olivier.miossec <olivier.miossec@metsys.fr>
2019-11-05 23:07:21 +0100

    correct publish info

--- publish.ps1
+++ publish.ps1
@@ -2,7 +2,7 @@
 $RootDir = "."
 
 $SrcDir = join-path -Path $RootDir -ChildPath "src"
-$ModulePath = join-path -Path $SrcDir -ChildPath "SrcDir"
+$ModulePath = join-path -Path $SrcDir -ChildPath "mpn-helper.psd1"
 
 try {
     Publish-Module -Path $ModulePath -NuGetApiKey $env:PSGALLERY_NUGET_API_KEY
