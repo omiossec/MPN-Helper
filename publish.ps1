6bfa8691a37e5ad677fa1c1ccb509a0698dbcc5c
olivier.miossec <olivier.miossec@metsys.fr>
2019-11-05 23:14:06 +0100

    Publish correction

--- publish.ps1
+++ publish.ps1
@@ -2,10 +2,10 @@
 $RootDir = "."
 
 $SrcDir = join-path -Path $RootDir -ChildPath "src"
-$ModulePath = join-path -Path $SrcDir -ChildPath "mpn-helper.psd1"
+
 
 try {
-    Publish-Module -Path $ModulePath -NuGetApiKey $env:PSGALLERY_NUGET_API_KEY
+    Publish-Module -Path $SrcDir -NuGetApiKey $env:PSGALLERY_NUGET_API_KEY
 }
 catch {
     Write-Error -Message " Exception Type: $($_.Exception.GetType().FullName) $($_.Exception.Message)"
