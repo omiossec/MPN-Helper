5793c5a8febd7f32ee7f0df9d504b910b2803932
olivier.miossec <olivier.miossec@metsys.fr>
2019-11-05 23:22:30 +0100

    Publish module

--- publish.ps1
+++ publish.ps1
@@ -3,9 +3,15 @@ $RootDir = "."
 
 $SrcDir = join-path -Path $RootDir -ChildPath "src"
 
+ new-item -name "mpn-helper" -ItemType Directory
+
+ $publishPath =  join-path -Path $RootDir -ChildPath "mpn-helper"
+
+ copy-item -path $SrcDir -Destination  $publishPath -Recurse
+
 
 try {
-    Publish-Module -Path $SrcDir -NuGetApiKey $env:PSGALLERY_NUGET_API_KEY
+    Publish-Module -Path $publishPath -NuGetApiKey $env:PSGALLERY_NUGET_API_KEY
 }
 catch {
     Write-Error -Message " Exception Type: $($_.Exception.GetType().FullName) $($_.Exception.Message)"
